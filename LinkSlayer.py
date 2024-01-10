cca6a4bae863c4dc2b925ab7a926abe8abdc062d
David <davite3@gmail.com>
2024-01-10 16:21:55 +0000

    cleaning + no emoji CLI flag

--- LinkSlayer.py
+++ LinkSlayer.py
@@ -1,129 +1,171 @@
 # Welcome to Link Slayer. Please read README.md for more information
-def menu_select():
-    while True:
-        try:
-            menu = int(input("\n\U0001F525 What would you like to do? \U0001F525\n \
-                \n\U0001F5251. Read a file in this directory \U0001F525 \
-                \n\U0001F5252. Read a file from the terminal \U0001F525 \
-                \n\U0001F5253. Read a file from this directory and append it to another file in this directory \U0001F525 \
-                \n\U0001F5254. Exit  \U0001F525\n"))
-            return menu
-        except TypeError:
-            print("\nError: Invalid character entered. Please enter a single digit integer\n")
-            menu = int(input("\n\U0001F525 What would you like to do? \U0001F525\n"))
-            
-            
-def intake_name(filename : str):
-    '''Function to intake a filename and open the file in read mode. If the file does not exist the function
-    will be repeatedly ask for a valid filename. It return a list, with each element being a line from the file'''
-    while True:
-        try:
-            readfile = open(filename, "r", encoding='utf-8')
-            lines = readfile.readlines()
-            readfile.close()
-            return lines
-        except FileNotFoundError:
-            filename = input("\nFile not found, please enter the name of a file within this directory \n")
-            
-def new_txt(new_name : str):
-    '''Function to intake the new file name and open the file in write mode. Returns the file object'''
-    while True:
-        try:
-            newfile = open(new_name, "w", encoding='utf-8')
-            return newfile
-        except FileExistsError:
-            new_name = input("\nInvalid File name. Try again.\n")
-            
+
+import os
+import sys
+from typing import List
+
+MAIN_EMOJI = "\U0001F525" * ("--no-emojis" not in sys.argv)
+
+
+def bidirectional_pad(text: str, pad: str, num: int = 1) -> str:
+    return (pad * num) + text + (pad * num)
+
+
+def menu_select() -> str:
+    return input(
+        bidirectional_pad("What would you like to do?", MAIN_EMOJI, 3)
+        + "\n"
+        + bidirectional_pad("1. Read a file", MAIN_EMOJI)
+        + "\n"
+        + bidirectional_pad("2. Read a text from the stdin", MAIN_EMOJI)
+        + "\n"
+        + bidirectional_pad(
+            "3. Read a file and append it to another file",
+            MAIN_EMOJI,
+        )
+        + "\n"
+        + bidirectional_pad("4. Exit", MAIN_EMOJI)
+        + "\n"
+    )
+
+
+def intake_name(filename: str):
+    """
+    Function to intake a filename and open the file in read mode.
+    If the file does not exist the function
+    will be repeatedly ask for a valid filename.
+    It return a list, with each element being a line from the file
+    """
+    while not os.path.isfile(filename):
+        filename = input(
+            bidirectional_pad(
+                "File not found, please enter the name of the file",
+                MAIN_EMOJI,
+            )
+        )
+
+    with open(filename, "r", encoding="utf-8") as readfile:
+        lines = readfile.readlines()
+        return lines
+
+
 def file_amalgam(lines):
-    '''Function to intake a filename and open the file in append mode. If the file does not exist the function
-    will be repeatedly ask for a valid filename.'''
-    
-    while True:
-        try:
-            
-            destination = open(input("\n\U0001F525 Please enter the name of the new text file you wish to add this file to \U0001F525 \n(This will overwrite any existing file of the same name) \n"), "a")
-            slayer(destination, lines)
-            
-            return
-        except FileNotFoundError:
-            
-            destination = open(input("\n\U0001F525 File Not Found. Please enter the name of the new text file you wish to add this file to \U0001F525 \n(This will overwrite any existing file of the same name) \n"), "a")
-    
-            
-def slayer(newfile, lines):
-    '''Function to intake a file object and a list of elements making of the contents of a previously read file.
+    """
+    Function to intake a filename and open the file in append mode.
+    If the file does not exist the function
+    will be repeatedly ask for a valid filename.
+    """
+    path = input(
+        bidirectional_pad(
+            "Please enter the name of the file you wish to add to",
+            MAIN_EMOJI,
+        )
+        + "\n (This will overwrite any existing file of the same name) \n"
+    )
+
+    while not os.path.isfile(path):
+        path = input(
+            bidirectional_pad(
+                "Please enter the name of the file you wish to add to",
+                MAIN_EMOJI,
+            )
+            + "\n (This will overwrite any existing file of the same name) \n"
+        )
+
+    with open(
+        path,
+        "a",
+        encoding="utf-8",
+    ) as destination:
+        new_data = slayer(lines)
+        destination.writelines(new_data)
+
+
+# This should return the text, not write it to a file.
+def slayer(lines) -> List[str]:
+    """
+    Intake a file object and a list of elements making of the contents of a previously read file.
     Writes the contents of the list to the new file, unless a line contains a keyphrase that is meant to be excluded
-    Returns the number of links removed'''
-    
-    key = '%LOCAL_FILE%'
-    key2 = 'remnote-user'
+    Returns the number of links removed
+    """
+
+    key = "%LOCAL_FILE%"
+    key2 = "remnote-user"
 
     link_count = 0
-
+    new_text = []
     for line in lines:
-        if key not in str(line):
-            if key2 not in str(line):
-                newfile.write(str(line))
+        line = str(line)
+        if key not in line:
+            if key2 not in line:
+                new_text.append(line)
             else:
                 link_count += 1
         else:
             link_count += 1
-        
-    newfile.close()
-        
-    return link_count
-        
 
-print("\n\U0001F525 WELCOME TO LINK SLAYER \U0001F525\n")
+    print(bidirectional_pad(f"{link_count} link(s) have been slain!", MAIN_EMOJI))
+    return new_text
 
-menu = 0
 
-while menu != 4:
-    
-    menu = menu_select()
-    
-    match menu:
-        
-        case 1:
-            #intakes name of file to be read       
-            filename = input("\n\U0001F525 Please enter the name of the text file you wish to cleanse of its sins \U0001F525 \n")
+def write_new_file(data: str):
+    filename = input(
+        bidirectional_pad(
+            "Please enter the name of the new text file that will be summoned",
+            MAIN_EMOJI,
+        )
+    )
+    while os.path.isfile(filename):
+        filename = input("\nFile already exists. Pick a different one.\n")
+    with open(filename, "w", encoding="utf-8") as newfile:
+        new_data = slayer(data)
+        newfile.writelines(new_data)
 
-            lines = intake_name(filename)
 
-            #intakes name of file to be written to
-            new_name = input("\n\U0001F525 Please enter the name of the new text file that will be summoned \U0001F525 \n(This will overwrite any existing file of the same name) \n")
+def main():
+    print(bidirectional_pad("WELCOME TO LINK SLAYER", MAIN_EMOJI))
+    while (menu := menu_select()) != "4":
+        match menu:
+            case "1":
+                # intakes name of file to be read
+                filename = input(
+                    bidirectional_pad(
+                        "Please enter the name of the text file you wish to cleanse of its sins",
+                        MAIN_EMOJI,
+                    )
+                )
+                # repetition in case 2
+                lines = intake_name(filename)
+                write_new_file(lines)
 
-            newfile = new_txt(new_name)
+            case "2":
+                # intakes text to be read
+                # repetition in case 2
+                lines = str(
+                    input(
+                        "\n\U0001F525 Please paste the text you wish to vanquish links from into the terminal (Right Click) \U0001F525\n"
+                    )
+                )
+                write_new_file(lines)
 
-            link_count = slayer(newfile, lines)
-                    
-            print("\n\U0001F525", link_count ,"link(s) have been slain! \U0001F525\n")
-                
-        case 2:
-            #intakes text to be read       
+            case "3":
+                # appends new file to current file
+                filename = input(
+                    bidirectional_pad(
+                        "Please enter the name of the text file you wish to cleanse of its sins and join in union with another cleansed file",
+                        MAIN_EMOJI,
+                    )
+                )
 
-            lines = str(input("\n\U0001F525 Please paste the text you wish to vanquish links from into the terminal (Right Click) \U0001F525\n"))
+                lines = intake_name(filename)
 
-            #intakes name of file to be written to
-            new_name = input("\n\U0001F525 Please enter the name of the new text file that will be summoned \U0001F525 \n(This will overwrite any existing file of the same name) \n")
+            case "4":
+                print(bidirectional_pad("Fare thee well, Hero", MAIN_EMOJI))
+                break
 
-            newfile = new_txt(new_name)
+            case _:
+                print(bidirectional_pad("Not a valid menu option", MAIN_EMOJI))
 
-            link_count = slayer(newfile, lines)
-                    
-            print("\n\U0001F525", link_count ,"link(s) have been slain! \U0001F525\n")
-            
-        case 3:
-            #appends new file to current file
-            filename = input("\n\U0001F525 Please enter the name of the text file you wish to cleanse of its sins and join in union with another cleansed file\U0001F525 \n")
 
-            lines = intake_name(filename)
-            
-            
-        case 4:
-            print("\n\U0001F525 Fare thee well, Hero \U0001F525\n")
-            break    
-        
-        case _:
-            print("\n\U0001F525 Not a valid menu option \U0001F525\n")
-            
+if __name__ == "__main__":
+    main()
